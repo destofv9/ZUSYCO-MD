986746c01b6211578634c92d9442b8801eb55830
Natious <141484837+destofv9@users.noreply.github.com>
2024-04-28 08:36:05 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,10 +16,10 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NUMBER: "254794110821",
+  SUDO_NUMBERS : "254794110821,254790627796",
+  OWNER_NAME: "『  N E S H 』",
+  BOT_NAME : " N E S H",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
